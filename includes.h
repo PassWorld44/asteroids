0ab0895190f607f403ccb52433ff277db601c36d
Emile BONDU <emile.bondu@gmail.com>
2020-05-27 23:32:53 +0200

    histoire de tt metre sur github

---
+++ includes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <stack>
+#include <cmath>
+#include <deque>
+#include <random>
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Network.hpp"
+#include "SFML/Window.hpp"
+
+
+namespace enums
+{
+	enum class ActionKey
+	{
+		NOTHING, UP, LEFT, DOWN, RIGHT, CLOSE
+	};
+}
